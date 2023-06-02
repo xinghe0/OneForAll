854b20456f1f3f8f1135c347d51701b77a34a48a
xinghe <1545975282@qq.com>
2023-06-02 18:34:21 +0800

    接入naabu端口扫描工具

--- common/naabu/naabu.py
+++ common/naabu/naabu.py
@@ -8,18 +8,24 @@
 """
 import os
 import xml.etree.ElementTree as ET
+import requests
+from config.log import logger
+
+requests.packages.urllib3.disable_warnings()  # 屏蔽ssl报错
 
 def naabu_cmd(path):
     paths = str(path).split('.')[0]
     #common/naabu/naabu
-    command = f'./naabu -list {path} -top-ports 100 -nmap-cli \'nmap -sV -oX {paths}_nmap-output.xml\' -o {paths}_result.txt'
+    command = f'common/naabu/naabu -list {path} -top-ports 100 -nmap-cli \'nmap -sV -oX {paths}_nmap-output.xml\' -o {paths}_result.txt'
     os.system(command)
+    info(f'{paths}_nmap-output.xml')
 
 
 def info(path):
     tree = ET.parse(path)
     root = tree.getroot()
     result = []
+    fit = []
 
     # 遍历每个host节点，获取地址、端口和服务名称信息
     for host in root.findall('host'):
@@ -28,9 +34,26 @@ def info(path):
             portid = port.get('portid')
             service_name = port.find('service').get('name')
             product_name = port.find('service').get('product')
-            result.append(f'{address}:{portid}')
-    print(result)
+            if service_name == "http":
+                result.append(f'{address}:{portid}')
+            else:
+                fit.append(f'{address}:{portid}')
+                http_live = http(fit)
+                if http_live:
+                    result.append(http(fit))
+    logger.log("INFOR",f'Http server list: {result}')
     return result
+
+def http(url_list):
+    http_list = ['http://','https://']
+    for i in http_list:
+        for j in url_list:
+            try:
+                result = requests.get(url=i + j,timeout=3)
+                if result.status_code in range(600):
+                    return j
+            except Exception as e:
+                pass
 
 
 if __name__ == '__main__':
